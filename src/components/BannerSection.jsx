66e2865a68345450ccf7a7fa10b3b8913c64de1c
Rajput <devendra.rajput.ext@nokia.com>
2020-10-04 20:23:14 +0530

    Complete home page

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,42 +1,66 @@
-import React from 'react'
-import {Button} from './Button';
-import {Link} from 'react-router-dom';
+import React from 'react';
 import './BannerSection.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 
 function BannerSection({
-    LightBg, topline, LightText, LightTextDesc, headline, description, buttonlabel, img, alt, imgStart
+  lightBg,
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart
 }) {
-    return (
-        <>
-            <div className={LightBg ? 'home__banner-section' : 'home__banner-section darkBg'}>
-                <div className="container">
-                    <div className="row home__banner-row" style={{display: "flex", flexDirection: imgStart == 'start' ?
-                    'row-reverse' : 'row'}}>
-
-                        <div className="col">
-                            <div className="home__banner-text-wrapper">
-                                <div className="top-line">{topline}</div>
-                                <h1 className={LightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                                <p className={LightTextDesc ? 'home__banner-subtitle' : 'home__banner-subtitle dark'}>{description}</p>
-
-                                <Link to="/sign-up">
-                                    <Button buttonSize='btn--wide' buttonColor="blue">{buttonlabel}</Button>
-                                </Link>
-
-                            </div>
-                        </div>
-
-                        <div className="col">
-                            <div className="home__banner-img-wrapper">
-                                <img src={img} alt={alt} className="home__banner-img"></img>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        className={lightBg ? 'home__Banner-section' : 'home__Banner-section darkBg'}
+      >
+        <div className='container'>
+          <div
+            className='row home__Banner-row'
+            style={{
+              display: 'flex',
+              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
+            }}
+          >
+            <div className='col'>
+              <div className='home__Banner-text-wrapper'>
+                <div className='top-line'>{topLine}</div>
+                <h1 className={lightText ? 'heading' : 'heading dark'}>
+                  {headline}
+                </h1>
+                <p
+                  className={
+                    lightTextDesc
+                      ? 'home__Banner-subtitle'
+                      : 'home__Banner-subtitle dark'
+                      
+                  }
+                >
+                  {description}
+                </p>
+                <Link to='/sign-up'>
+                  <Button buttonSize='btn--wide' buttonColor='blue'>
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </div>
             </div>
-        </>
-    )
+            <div className='col'>
+              <div className='home__Banner-img-wrapper'>
+                <img src={img} alt={alt} className='home__Banner-img' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BannerSection;
--- src/components/pages/Homepage/Home.jsx
+++ src/components/pages/Homepage/Home.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 import BannerSection from '../../BannerSection';
-import {homeObjOne} from './Data';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+// import Pricing from '../../Pricing';
 
 function Home() {
-    return (
-        <>
-            <BannerSection {...homeObjOne} />
-        </>
-    )
+  return (
+    <>
+      <BannerSection {...homeObjOne} />
+      <BannerSection {...homeObjThree} />
+      <BannerSection {...homeObjTwo} />
+      {/* <Pricing /> */}
+      <BannerSection {...homeObjFour} />
+    </>
+  );
 }
 
 export default Home;
